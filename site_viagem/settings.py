f8f42dff47080ea671800523678787879329157c
eliascomh <grilocamusiano@gmail.com>
2022-11-12 14:57:02 -0300

    widget e calendário - tempus-dominus

--- site_viagem/settings.py
+++ site_viagem/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tempus_dominus',
     'passagens',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPUS_DOMINUS_LOCALIZE = True
